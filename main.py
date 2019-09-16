53767a69f36f6cfcf2e883c2acdfa5164f7cf392
PARSONS Charlie <cparsons@altranuk.local>
2019-09-16 09:37:41 +0100

    Added Git Ignore File

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import json
+import requests
+
 def main():
-    print("Welcome to BusBoard.")
+    chooseBus = input("Please enter a bus code: ")
+    response = requests.get('https://transportapi.com/v3/uk/bus/stop/'+chooseBus+'/live.json?app_id=06c96df7&app_key=2927c9980ab1aec88eebede64029c064&group=route&nextbuses=no')
+    test = response.text
+    parsedJSON = json.loads(test)
+    print(json.dumps(parsedJSON, indent=4))
 
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
