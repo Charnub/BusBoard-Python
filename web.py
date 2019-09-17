2fe16b1e25ec01ecdd999e0e9cf1ed166e78cfce
cparsons <parsons671@gmail.com>
2019-09-17 16:09:04 +0100

    Moved most of the functions from main.py to respectable functions within other files. These files are then imported into web.py so that it can use the created functions and classes to output Bus times and the weather to a website once given a postcode.

--- web.py
+++ web.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
-from main import GetBusStops, GetPostcodeInfo
+
+import BusStop
+
+from transportQuery import PostcodeQuery
+from BusStop import BusStop
+from weather import Weather
 
 app = Flask(__name__)
 
@@ -14,33 +19,19 @@ def index():
 @app.route("/busInfo")
 def busInfo():
     postcode = request.args.get('postcode')
-    postcode_dict = GetPostcodeInfo(postcode, postcode_endpoint)
+    postcode_dict = PostcodeQuery.GetPostcodeInfo(postcode, postcode_endpoint)
 
     post_long = postcode_dict['longitude']
     post_lat = postcode_dict['latitude']
 
-    list_bus_stops = GetBusStops(post_lat, post_long, stop_endpoint)
+    weather = Weather(post_lat, post_long)
+
+    list_bus_stops = BusStop.GetBusStops(post_lat, post_long, stop_endpoint)
 
     for stop in list_bus_stops:
         stop.GetBuses(bus_endpoint)
 
-    # bus_stop_1 = list_bus_stops[0].buses
-    # bus_stop_2 = list_bus_stops[1].buses
-    #
-    # [bus_numbers_1, bus_directions_1, bus_departures_1] = [[], [], []]
-    # for bus in bus_stop_1:
-    #     bus_numbers_1.append(bus.line_name)
-    #     bus_directions_1.append(bus.direction)
-    #     bus_departures_1.append(bus.dep_time)
-    #
-    # [bus_numbers_2, bus_directions_2, bus_departures_2] = [[], [], []]
-    # for bus in bus_stop_2:
-    #     bus_numbers_2.append(bus.line_name)
-    #     bus_directions_2.append(bus.direction)
-    #     bus_departures_2.append(bus.dep_time)
-
-
-    return render_template('info.html', postcode=postcode, stops=list_bus_stops)
+    return render_template('info.html', postcode=postcode, stops=list_bus_stops, weather=weather.forecast)
 
 
 if __name__ == "__main__":
